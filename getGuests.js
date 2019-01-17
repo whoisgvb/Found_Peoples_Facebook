dda5e7ba644e4c239b24666ccdb046298043bedd
Gustavo Vilela <gvbsec@protonmail.com>
2019-01-17 00:04:03 -0200

    version 1.0 with photos and add friend button

---
+++ getGuests.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><script type="text/javascript">// <![CDATA[
+$(document).ready(function(){
+	$(".fcb a").each(function() {$("#names").append($(this).html()+"</br>")});
+});
+// ]]></script></pre>
+<div id="names"></div>
+<pre>
+
